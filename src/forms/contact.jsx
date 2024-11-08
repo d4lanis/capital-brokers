26d217d12eeeab123b2b545ffccb20b2b4067c95
Daniel Alanis <daniel.alanis.hdz@gmail.com>
2024-11-08 13:53:21 -0600

    add a basic contact form

--- src/forms/contact.jsx
+++ src/forms/contact.jsx
@@ -1,9 +1,15 @@
-import { Button, Text, TextInputField, Pane } from "evergreen-ui";
+import {
+  Button,
+  Text,
+  TextInputField,
+  TextareaField,
+  Pane,
+} from "evergreen-ui";
 import { useState } from "react";
+
 
 // eslint-disable-next-line react/prop-types
 const ContactForm = ({ handleClose }) => {
-
   const [data, setData] = useState({
     name: "",
     phone: "",
@@ -14,37 +20,40 @@ const ContactForm = ({ handleClose }) => {
   const handleChange = (e) => {
     const { name, value } = e.target;
     setData((prev) => ({ ...prev, [name]: value }));
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(data);
 
     // Close the dialog after form submission
-    //handleClose(false);
+    handleClose(false);
   };
 
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
-      <TextInputField
-        label={<Text color="white">Nombre Completo</Text>}
-        placeholder="Ingrese su nombre completo"
-        style={{ color: "white", backgroundColor: "#2a2d3b" }}
-        value={data.name}
-        name="name"
-        onChange={handleChange}
-        required
-      />
-
-      <TextInputField
-        label={<Text color="white">Teléfono</Text>}
-        placeholder="Ingrese su número de teléfono"
-        style={{ color: "white", backgroundColor: "#2a2d3b" }}
-        value={data.phone}
-        name="phone"
-        onChange={handleChange}
-        required
-      />
+    <form onSubmit={handleSubmit}>
+      <Pane display="flex" gap={16}>
+        <TextInputField
+          label={<Text color="white">Nombre Completo</Text>}
+          placeholder="Ingrese su nombre completo"
+          style={{ color: "white", backgroundColor: "#2a2d3b" }}
+          value={data.name}
+          name="name"
+          onChange={handleChange}
+          required
+          flex={1}
+        />
+        <TextInputField
+          label={<Text color="white">Teléfono</Text>}
+          placeholder="Ingrese su número de teléfono"
+          style={{ color: "white", backgroundColor: "#2a2d3b" }}
+          value={data.phone}
+          name="phone"
+          onChange={handleChange}
+          required
+          flex={1}
+        />
+      </Pane>
 
       <TextInputField
         label={<Text color="white">Correo Electrónico</Text>}
@@ -57,10 +66,20 @@ const ContactForm = ({ handleClose }) => {
         required
       />
 
+      <TextareaField
+        label={<Text color="white">Mensaje</Text>}
+        placeholder="Escriba su mensaje aquí"
+        style={{ color: "white", backgroundColor: "#2a2d3b" }}
+        value={data.message}
+        name="message"
+        onChange={handleChange}
+        required
+      />
+
       <Pane display="flex" justifyContent="flex-end" marginTop={20}>
         <Button
           appearance="default"
-          onClick={() => handleClose(false)} // Close dialog on cancel
+          onClick={() => handleClose(false)}
           marginRight={10}
         >
           Cancelar
