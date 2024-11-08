062bd1da93f8fc78c1bdb49300a81aa339cfec02
Daniel Alanis <daniel.alanis.hdz@gmail.com>
2024-11-08 13:54:22 -0600

    modify the footer to add social media icons, location of the business and legal info as terms and conditions, etc

--- src/components/ui/footer.jsx
+++ src/components/ui/footer.jsx
@@ -1,30 +1,59 @@
 import { Pane, Text, majorScale, Link } from "evergreen-ui";
+import { socialMedia } from "../../const";
 
 const Footer = () => {
   return (
     <Pane
-      // Removed position: fixed
-      bottom={0} // This can be removed as well
-      left={0} // This can be removed as well
-      width="100%" // Makes it span the full width
-      background="#101840" // Dark background color
-      padding={majorScale(2)} // Adds padding
+      background="#101840"
+      padding={majorScale(4)}
       display="flex"
       flexDirection="column"
-      justifyContent="center" // Center-aligns content
       alignItems="center"
-      zIndex={1} // Keeps it above other elements
+      textAlign="center"
     >
-      <Text size={400} color="white" marginBottom={majorScale(1)}>
-        &copy; {new Date().getFullYear()} BrandName. Todos los derechos reservados. | Author: N-TECH Soluciones IT
-      </Text>
-      <Pane display="flex" marginTop={majorScale(1)}>
-        <Link href="/aviso-de-privacidad" color="white" marginRight={majorScale(2)}>
-          Aviso de Privacidad
-        </Link>
-        <Link href="/terminos-y-condiciones" color="white">
-          Términos y Condiciones
-        </Link>
+      {/* Main Row with Social Media, Contact, and Legal Sections */}
+      <Pane
+        display="flex"
+        justifyContent="space-around"
+        width="100%"
+        maxWidth="800px"
+        color="white"
+        marginBottom={majorScale(3)}
+      >
+        {/* Social Media Section */}
+        <Pane display="flex" alignItems="center" gap={16}>
+          {socialMedia.map((item) => (
+            <Link href={item.link} target="_blank" key={item.name} color="white">
+              <item.icon size={24} />
+            </Link>
+          ))}
+        </Pane>
+
+        {/* Contact Information Section */}
+        <Pane display="flex" flexDirection="column" alignItems="center">
+          <Text color="white">Teléfono: +52 844 289 9086</Text>
+          <Text color="white">Ubicación: Saltillo, Coahuila. México</Text>
+        </Pane>
+
+        {/* Legal Section */}
+        <Pane display="flex" flexDirection="column" alignItems="center">
+          <Link href="/terminos-y-condiciones" color="white" marginBottom={4}>
+            Términos y Condiciones
+          </Link>
+          <Link href="/aviso-de-privacidad" color="white">
+            Aviso de Privacidad
+          </Link>
+        </Pane>
+      </Pane>
+
+      {/* Branding Section */}
+      <Pane>
+        <Text size={400} color="white">
+          &copy; {new Date().getFullYear()} Capital Brokers Financial Group. Todos los derechos reservados.
+        </Text>
+        <Text size={300} color="white" marginTop={majorScale(1)}>
+          Author: <Link href="http://www.ntechsolucionesit.com" target="_blank" color="white">Daniel Alanis</Link>
+        </Text>
       </Pane>
     </Pane>
   );
